385625360714d92438330c4ff049ebef2590917b
Trinath Mallavarapu <trinathm@outlook.com>
2017-03-13 12:21:29 +0530

    added option for user to select number he want to count for

--- RoleDice/MaxRolls.cs
+++ RoleDice/MaxRolls.cs
@@ -15,19 +15,29 @@ namespace RoleDice
         /// </summary>
         public void Run()
         {
-            int totalDice = Convert.ToInt32(Console.ReadLine());
-            int maxValue = totalDice * NUM_SIDES;
-            int totalAttempts = 1;
             while (true)
             {
-                int rolledValue = this.GetNextNumber(totalDice);
-                if (rolledValue == maxValue) break;
-                Console.WriteLine("Rolled " + rolledValue);
-                totalAttempts++;
-            }
+                Console.WriteLine("Enter number of dice or press -1 to exit!!");
+                int totalDice = Convert.ToInt32(Console.ReadLine());
+                int maxValue = 0;
+                do
+                {
+                    Console.WriteLine("Enter the valid number you want to try : ");
+                    maxValue = Convert.ToInt32(Console.ReadLine());
+                } while (maxValue < totalDice || maxValue > totalDice * NUM_SIDES);
 
-            Console.WriteLine(string.Format("Rolled {0} after {1} attempts", maxValue, totalAttempts));
-            Console.ReadLine();
+                if (totalDice < 0) break;
+                int totalAttempts = 1;
+                while (true)
+                {
+                    int rolledValue = this.GetNextNumber(totalDice);
+                    if (rolledValue == maxValue) break;
+                    Console.WriteLine("Rolled " + rolledValue);
+                    totalAttempts++;
+                }
+
+                Console.WriteLine(string.Format("Rolled {0} after {1} attempts", maxValue, totalAttempts));
+            }
         }
 
         private int GetNextNumber(int totalDice)
